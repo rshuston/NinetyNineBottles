9fec479fd401c17634c86661c7593a1147e5d22a
Robert Huston <robert.huston@pinpointdynamics.com>
2021-08-04 13:45:28 -0400

    Initial Commit

---
+++ NinetyNineBottles/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  NinetyNineBottles
+//
+//  Created by Robert Huston on 8/4/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
